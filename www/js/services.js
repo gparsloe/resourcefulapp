093f32f2d46a45344380537de43ad1fb4465571b
Gail <gparsloe@umass.edu>
2017-08-13 22:22:47 -0400

    removed a couple of images. added more tools.

--- www/js/services.js
+++ www/js/services.js
@@ -7,7 +7,7 @@ angular.module('starter.services', [])
   var tools = [{
     id: 0,
     name: 'Gratitude',
-    lastText: 'Think of 5 things that you are grateful for.',
+    lastText: 'Think of 5 things that you are grateful for. Consider sending someone a thank you email or mailing them a card.',
     face: 'img/buds.jpg'
   }, {
     id: 1,
@@ -22,23 +22,63 @@ angular.module('starter.services', [])
   }, {
     id: 3,
     name: 'Cut off negative thoughts',
-    lastText: 'Pull out that sword!',
+    lastText: 'Pull out that sword. Tame your beasty mind!',
     face: 'img/sword1.jpg'
   }, {
     id: 4,
-    name: 'Make time to breathe. It\s worth it!',
-    lastText: 'This is wicked good ice cream.',
+    name: 'Make time to breathe. ',
+    lastText: 'It\s worth it!',
     face: 'img/turtle1.jpg'
   }, {
     id: 5,
-    name: 'Are you contracted? If yes, expand. Stand up straight. Pull your shoulders back. Look up. Smile.',
-    lastText: '',
+    name: 'Expand. ',
+    lastText: 'Stand up straight. Pull your shoulders back. Look up. Smile.',
     face: 'img/expand.jpg'
   }, {
     id: 6,
     name: 'Do something nice for someone else.',
     lastText: '',
+    face: 'img/hands1.jpg'
+  }, {
+    id: 7,
+    name: 'Take care of something that needs to get done.',
+    lastText: '',
+    face: 'img/todo1.jpg'
+  }, {
+    id: 8,
+    name: 'Clean something for 10 minutes.',
+    lastText: '',
+    face: 'img/dishes1.jpg'
+  }, {
+    id: 9,
+    name: 'Listen to music that shifts your state.',
+    lastText: '',
+    face: 'img/listen1.jpg'
+  }, {
+    id: 10,
+    name: 'Massage your own feet or shoulders.',
+    lastText: '',
+    face: 'img/feetInWater.jpg'
+  }, {
+    id: 11,
+    name: 'Pay attention externally. ',
+    lastText: 'Look all that\s around you, get outside of your mind. Name 3 things that you see, 3 things that you hear, 3 things that you feel. Look up and smile.',
     face: 'img/flower1.jpg'
+  }, {
+    id: 12,
+    name: 'Take a bath.',
+    lastText: 'Enjoy the preparation and anticipation of climbing in.',
+    face: 'img/HappyBabyBath.jpg'
+  }, {
+    id: 13,
+    name: 'Connect with a friend.',
+    lastText: 'If no one is around to be with or to talk to, imagine them in your mind and what you love about them. Consider sending them an email and telling them how much you love them. Feel free to be very specific about what you love about them.',
+    face: 'img/call1.jpg'
+  }, {
+    id: 14,
+    name: 'Get outside.',
+    lastText: '',
+    face: 'img/flower3.jpg'
   }];
 
   return {
